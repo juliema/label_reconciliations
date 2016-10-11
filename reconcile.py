84dff1ab5a0854f180ead96c5eed81fd3b233d46
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2016-10-11 14:31:49 -0400

    Combine two old scripts into one #4

--- reconcile.py
+++ reconcile.py
@@ -2,6 +2,7 @@ import re
 import sys
 import json
 import argparse
+import dateutil
 import pandas as pd
 from collections import Counter, namedtuple
 from itertools import combinations
@@ -21,7 +22,7 @@ SELECT_NO_MATCH = '<No match on select>'
 TEXT_NO_MATCH = '<No match on text>'
 NOT_ENOUGH_TO_MATCH = '<Only one transcript>'
 
-# Useful for naming data fields, rather than using indexes into tuples
+# Useful for naming tuple items, rather than using indexes into tuples
 ExactScore = namedtuple('ExactScore', 'value count')
 FuzzyRatioScore = namedtuple('FuzzyRatioScore', 'score value')
 FuzzySetScore = namedtuple('FuzzySetScore', 'score value tokens')
@@ -29,6 +30,97 @@ FuzzySetScore = namedtuple('FuzzySetScore', 'score value tokens')
 UNWANTED_COLUMNS = ['subject_data', 'subject_retired', 'subject_subjectId']
 COLUMNS_WITH_BAD_DATA = {}  # Subject_* columns with non-identical values in a group
 ARGS = None
+
+
+def extract_json_value(subject_json, column=''):
+    if column in list(subject_json.values())[0]:
+        return list(subject_json.values())[0][column]
+
+
+def extract_json_date(metadata_json, column=''):
+    return dateutil.parser.parse(metadata_json[column]).strftime('%d-%b-%Y %H:%M:%S')
+
+
+def header_label(task_id, label, task_type):
+    return '{}{:0>3}{}: {}'.format(task_id[0], task_id[1:], task_type, label)
+
+
+def extract_an_annotaion(df, task, task_id, index):
+    if isinstance(task.get('value'), list):
+        for subtask in task['value']:
+            subtask_id = subtask.get('task', task_id)
+            extract_an_annotaion(df, subtask, subtask_id, index)
+    elif task.get('select_label'):
+        header = header_label(task_id, task['select_label'], 's')
+        value = task.get('label')
+        df.loc[index, header] = value
+    elif task.get('task_label'):
+        header = header_label(task_id, task['task_label'], 't')
+        value = task.get('value')
+        df.loc[index, header] = value
+    else:
+        print('Error: Could not parse the annotations.')
+        sys.exit()
+
+
+def extract_annotations_json(df):
+    df['annotation_json'] = df['annotations'].map(lambda x: json.loads(x))
+    for index, row in df.iterrows():
+        for task in row['annotation_json']:
+            task_id = task['task']
+            extract_an_annotaion(df, task, task_id, index)
+    df.drop('annotation_json', axis=1, inplace=True)
+
+
+def extract_subject_json(df):
+    df['subject_json'] = df['subject_data'].map(lambda x: json.loads(x))
+    subject_keys = {}
+    for subj in df['subject_json']:
+        for val in iter(subj.values()):
+            for k in val.keys():
+                subject_keys[k] = 1
+    for k in subject_keys.keys():
+        df['subject_' + k] = df['subject_json'].apply(extract_json_value, column=k)
+    df.drop('subject_json', axis=1, inplace=True)
+
+
+def extract_metata_json(df):
+    df['metadata_json'] = df['metadata'].map(lambda x: json.loads(x))
+    df['classification_started_at'] = df['metadata_json'].apply(extract_json_date, column='started_at')
+    df['classification_finished_at'] = df['metadata_json'].apply(extract_json_date, column='finished_at')
+    df.drop('metadata_json', axis=1, inplace=True)
+
+
+def expand():
+    global ARGS
+    subjects_df = pd.read_csv(ARGS.input_subjects)
+    df = pd.read_csv(ARGS.input_classifications)
+
+    # We need to do this by workflow because each one's annotations have a different structure
+    df = df.loc[df.workflow_id == ARGS.workflow_id, :]
+
+    # bring the last column to be the first
+    cols = df.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    df = df[cols]
+
+    # Make key data types match in the two data frames
+    df['subject_ids'] = df.subject_ids.map(lambda x: int(str(x).split(';')[0]))
+
+    # Get subject info we need from the subjects_df
+    df = pd.merge(df, subjects_df[['subject_id', 'locations']], how='left',
+                  left_on='subject_ids', right_on='subject_id')
+
+    extract_metata_json(df)
+    extract_annotations_json(df)
+    extract_subject_json(df)
+
+    df.drop(['user_id', 'user_ip', 'subject_id'], axis=1, inplace=True)
+    df.rename(columns={'subject_ids': 'subject_id'}, inplace=True)
+    df.sort_values(['subject_id', 'classification_id'], inplace=True)
+
+    if ARGS.unreconciled:
+        df.to_csv(ARGS.unreconciled, sep=',', index=False, encoding='utf-8')
 
 
 def output(best, match_type, count=None, total=None, score=None):
@@ -123,49 +215,84 @@ def reconcile_text(group):
     return output(NO_MATCHES, TEXT_NO_MATCH)
 
 
-def reconcile():
+def reconcile(unreconciled_df):
     global ARGS
-    df = pd.read_csv(ARGS.input)
 
     # How to aggregate the columns based on each column's type which is determined by the column name
-    select_cols = {c: reconcile_select for c in df.columns if re.match(r'T\d+s:', c)}
-    text_cols = {c: reconcile_text for c in df.columns if re.match(r'T\d+t:', c)}
-    subject_cols = {c: reconcile_same for c in df.columns if c.startswith('subject_') and c != GROUP_BY}
+    select_cols = {c: reconcile_select for c in unreconciled_df.columns if re.match(r'T\d+s:', c)}
+    text_cols = {c: reconcile_text for c in unreconciled_df.columns if re.match(r'T\d+t:', c)}
+    subject_cols = {c: reconcile_same for c in unreconciled_df.columns if c.startswith('subject_') and c != GROUP_BY}
     aggregate_cols = dict(list(select_cols.items()) + list(text_cols.items()) + list(subject_cols.items()))
     aggregate_cols['locations'] = reconcile_same
     aggregate_cols = {k: v for k, v in aggregate_cols.items() if k not in UNWANTED_COLUMNS}
 
     # Aggregate using the per column functions setup above
-    grouped = df.fillna('').groupby(GROUP_BY).aggregate(aggregate_cols)
+    grouped_df = unreconciled_df.fillna('').groupby(GROUP_BY).aggregate(aggregate_cols)
 
-    grouped.drop(COLUMNS_WITH_BAD_DATA.keys(), axis=1, inplace=True)
+    grouped_df.drop(COLUMNS_WITH_BAD_DATA.keys(), axis=1, inplace=True)
 
     # Split the combined reconciled value and flag into separate columns
     for c in list(text_cols.keys()) + list(select_cols.keys()):
-        grouped[c + '_explanation'], grouped[c] = grouped[c].str.split(SEPARATOR, n=1).str
+        grouped_df[c + '_explanation'], grouped_df[c] = grouped_df[c].str.split(SEPARATOR, n=1).str
 
-    grouped = grouped.reindex_axis(sorted(grouped.columns), axis=1)
+    grouped_df = grouped_df.reindex_axis(sorted(grouped_df.columns), axis=1)
 
     # Make the sidecar explanations file -- DELETE these lines if we want one file for this
-    explanations = grouped.loc[:, [c for c in grouped.columns if c.endswith('_explanation')]]
-    grouped.drop([c for c in grouped.columns if c.endswith('_explanation')], axis=1, inplace=True)
+    explanations = grouped_df.loc[:, [c for c in grouped_df.columns if c.endswith('_explanation')]]
+    grouped_df.drop([c for c in grouped_df.columns if c.endswith('_explanation')], axis=1, inplace=True)
     if ARGS.explanations:
         explanations.to_csv(ARGS.explanations, sep=',', encoding='utf-8')
 
-    grouped.to_csv(ARGS.output, sep=',', encoding='utf-8')
+    grouped_df.to_csv(ARGS.reconciled, sep=',', encoding='utf-8')
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', required=True, help='The raw extracts CSV file to reconcile')
-    parser.add_argument('-o', '--output', required=True, help='Write the reconciled extracts to this CSV file')
-    parser.add_argument('-r', '--fuzzy-ratio-threshold', default=90, type=int,
+    parser = argparse.ArgumentParser(description='''
+        This takes raw Notes from Nature classifications and subjects files and creates a reconciliation
+        of the classifications for a particular workflow. That is, it reduces n classifications per
+        subject to the "best" values along with explanations of how these best values were determined.
+    ''')
+    parser.add_argument('-w', '--workflow-id', type=int, required=True,
+                        help='The workflow to extract (required).')
+    parser.add_argument('-c', '--input-classifications', required=True,
+                        help='The Notes from Nature classifications CSV input file (required).')
+    parser.add_argument('-s', '--input-subjects', required=True,
+                        help='The Notes from Nature subjects CSV input file (required).')
+    parser.add_argument('-r', '--reconciled',
+                        help='Write the reconciled classifications to this CSV file '
+                             '(default=reconciled_<workflow-id>.csv).')
+    parser.add_argument('-R', '--no-reconciled', action='store_true',
+                        help='Do not write either a reconciled classifications file or an explanations file and '
+                        'stop further processing. This requires the "-u" option.')
+    parser.add_argument('-u', '--unreconciled',
+                        help='Write the unreconciled workflow classifications to this CSV file.')
+    parser.add_argument('-f', '--fuzzy-ratio-threshold', default=90, type=int,
                         help='Sets the cutoff for fuzzy ratio matching (0-100, default=90). '
                              'See https://github.com/seatgeek/fuzzywuzzy.')
-    parser.add_argument('-s', '--fuzzy-set-threshold', default=50, type=int,
+    parser.add_argument('-F', '--fuzzy-set-threshold', default=50, type=int,
                         help='Sets the cutoff for fuzzy set matching (0-100, default=50). '
                              'See https://github.com/seatgeek/fuzzywuzzy.')
-    parser.add_argument('-e', '--explanations', help='Write reconciliation explanations to this file')
+    parser.add_argument('-e', '--explanations',
+                        help='Write reconciliation explanations to this file '
+                             '(default=reconciled_<workflow-id>_explanations.csv).')
+    parser.add_argument('-E', '--no-explanations', action='store_true',
+                        help='Do not create a reconciliation explanations file.')
+    parser.add_argument('-m', '--summary',
+                        help='Write a summary of the reconciliation to this file. '
+                             '(default=reconciled_<workflow-id>_summary.txt).')
+    parser.add_argument('-M', '--no-summary', action='store_true',
+                        help='Do not write a summary file.')
     ARGS = parser.parse_args()
+    if not ARGS.reconciled:
+        ARGS.reconciled = 'reconciled_{}.csv'.format(ARGS.workflow_id)
+    if not ARGS.explanations:
+        ARGS.explanations = 'reconciled_{}_explanations.csv'.format(ARGS.workflow_id)
+    if not ARGS.summary:
+        ARGS.explanations = 'reconciled_{}_summary.txt'.format(ARGS.workflow_id)
+    if ARGS.no_reconciled and not ARGS.unreconciled:
+        print('The --no-reconciled option (-R) requires the --unreconciled (-u) option.')
+        sys.exit()
 
-    reconcile()
+    unreconciled_df = expand()
+    if not ARGS.no_reconciled:
+        reconcile(unreconciled_df)
