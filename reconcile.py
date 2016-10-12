9a51fca12efc901bbc0bd51dde6e79510c59ed2e
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2016-10-12 09:49:10 -0400

    Change how blanks and singletons are handled #9

--- reconcile.py
+++ reconcile.py
@@ -4,23 +4,29 @@ import json
 import argparse
 import dateutil
 import pandas as pd
+from functools import reduce
 from collections import Counter, namedtuple
 from itertools import combinations
 from fuzzywuzzy import fuzz
+
 
 PLACE_HOLDERS = ['placeholder']  # Replace these placeholders with an empty string
 NO_MATCHES = '<NO MATCHES>'      # A flag for unmatched data
 GROUP_BY = 'subject_id'          # We group on this column
 SEPARATOR = ':'                  # Used to separate match flags from values
 
-# Type of matching
-EXACT_MATCH = 'Exact match'
-NORMALIZED_EXACT_MATCH = 'Normalized exact match'
-PARTIAL_RATIO_MATCH = 'Partial ratio match'
-TOKEN_SET_RATIO_MATCH = 'Token set ratio match'
-SELECT_NO_MATCH = '<No match on select>'
-TEXT_NO_MATCH = '<No match on text>'
-NOT_ENOUGH_TO_MATCH = '<Only one transcript>'
+
+class match_types:
+    all_blank = '<All are blank>'
+    one_transcript = '<Only one transcript>'
+
+    exact = 'Exact match'
+    select_no_match = '<No match on select>'
+
+    normalized_exact = 'Normalized exact match'
+    partial_ratio = 'Partial ratio match'
+    token_set_ratio = 'Token set ratio match'
+    text_no_match = '<No match on text>'
 
 # Useful for naming tuple items, rather than using indexes into tuples
 ExactScore = namedtuple('ExactScore', 'value count')
@@ -28,7 +34,7 @@ FuzzyRatioScore = namedtuple('FuzzyRatioScore', 'score value')
 FuzzySetScore = namedtuple('FuzzySetScore', 'score value tokens')
 
 UNWANTED_COLUMNS = ['subject_data', 'subject_retired', 'subject_subjectId']
-COLUMNS_WITH_BAD_DATA = {}  # Subject_* columns with non-identical values in a group
+# COLUMNS_WITH_BAD_DATA = {}  # Subject_* columns with non-identical values in a group
 ARGS = None
 
 
@@ -45,99 +51,84 @@ def header_label(task_id, label, task_type):
     return '{}{:0>3}{}: {}'.format(task_id[0], task_id[1:], task_type, label)
 
 
-def extract_an_annotaion(df, task, task_id, index):
+def extract_an_annotaion(unreconciled_df, task, task_id, index):
     if isinstance(task.get('value'), list):
         for subtask in task['value']:
             subtask_id = subtask.get('task', task_id)
-            extract_an_annotaion(df, subtask, subtask_id, index)
+            extract_an_annotaion(unreconciled_df, subtask, subtask_id, index)
     elif task.get('select_label'):
         header = header_label(task_id, task['select_label'], 's')
         value = task.get('label')
-        df.loc[index, header] = value
+        unreconciled_df.loc[index, header] = value
     elif task.get('task_label'):
         header = header_label(task_id, task['task_label'], 't')
         value = task.get('value')
-        df.loc[index, header] = value
+        unreconciled_df.loc[index, header] = value
     else:
         print('Error: Could not parse the annotations.')
         sys.exit()
 
 
-def extract_annotations_json(df):
-    df['annotation_json'] = df['annotations'].map(lambda x: json.loads(x))
-    for index, row in df.iterrows():
+def extract_annotations_json(unreconciled_df):
+    unreconciled_df['annotation_json'] = unreconciled_df['annotations'].map(lambda x: json.loads(x))
+    for index, row in unreconciled_df.iterrows():
         for task in row['annotation_json']:
             task_id = task['task']
-            extract_an_annotaion(df, task, task_id, index)
-    df.drop('annotation_json', axis=1, inplace=True)
+            extract_an_annotaion(unreconciled_df, task, task_id, index)
+    unreconciled_df.drop('annotation_json', axis=1, inplace=True)
 
 
-def extract_subject_json(df):
-    df['subject_json'] = df['subject_data'].map(lambda x: json.loads(x))
+def extract_subject_json(unreconciled_df):
+    unreconciled_df['subject_json'] = unreconciled_df['subject_data'].map(lambda x: json.loads(x))
     subject_keys = {}
-    for subj in df['subject_json']:
+    for subj in unreconciled_df['subject_json']:
         for val in iter(subj.values()):
             for k in val.keys():
                 subject_keys[k] = 1
     for k in subject_keys.keys():
-        df['subject_' + k] = df['subject_json'].apply(extract_json_value, column=k)
-    df.drop('subject_json', axis=1, inplace=True)
+        unreconciled_df['subject_' + k] = unreconciled_df['subject_json'].apply(extract_json_value, column=k)
+    unreconciled_df.drop('subject_json', axis=1, inplace=True)
 
 
-def extract_metata_json(df):
-    df['metadata_json'] = df['metadata'].map(lambda x: json.loads(x))
-    df['classification_started_at'] = df['metadata_json'].apply(extract_json_date, column='started_at')
-    df['classification_finished_at'] = df['metadata_json'].apply(extract_json_date, column='finished_at')
-    df.drop('metadata_json', axis=1, inplace=True)
+def extract_metata_json(unreconciled_df):
+    unreconciled_df['metadata_json'] = unreconciled_df['metadata'].map(lambda x: json.loads(x))
+    unreconciled_df['classification_started_at'] = unreconciled_df['metadata_json'].apply(extract_json_date, column='started_at')
+    unreconciled_df['classification_finished_at'] = unreconciled_df['metadata_json'].apply(extract_json_date, column='finished_at')
+    unreconciled_df.drop('metadata_json', axis=1, inplace=True)
 
 
-def expand():
-    global ARGS
-    subjects_df = pd.read_csv(ARGS.input_subjects)
-    df = pd.read_csv(ARGS.input_classifications)
+def expand(workflow_id, input_classifications, input_subjects):
+    subjects_df = pd.read_csv(input_subjects)
+    unreconciled_df = pd.read_csv(input_classifications)
 
     # We need to do this by workflow because each one's annotations have a different structure
-    df = df.loc[df.workflow_id == ARGS.workflow_id, :]
+    unreconciled_df = unreconciled_df.loc[unreconciled_df.workflow_id == workflow_id, :]
 
     # bring the last column to be the first
-    cols = df.columns.tolist()
+    cols = unreconciled_df.columns.tolist()
     cols = cols[-1:] + cols[:-1]
-    df = df[cols]
+    unreconciled_df = unreconciled_df[cols]
 
     # Make key data types match in the two data frames
-    df['subject_ids'] = df.subject_ids.map(lambda x: int(str(x).split(';')[0]))
+    unreconciled_df['subject_ids'] = unreconciled_df.subject_ids.map(lambda x: int(str(x).split(';')[0]))
 
     # Get subject info we need from the subjects_df
-    df = pd.merge(df, subjects_df[['subject_id', 'locations']], how='left',
-                  left_on='subject_ids', right_on='subject_id')
+    unreconciled_df = pd.merge(unreconciled_df, subjects_df[['subject_id', 'locations']], how='left',
+                               left_on='subject_ids', right_on='subject_id')
 
-    extract_metata_json(df)
-    extract_annotations_json(df)
-    extract_subject_json(df)
+    extract_metata_json(unreconciled_df)
+    extract_annotations_json(unreconciled_df)
+    extract_subject_json(unreconciled_df)
 
-    df.drop(['user_id', 'user_ip', 'subject_id'], axis=1, inplace=True)
-    df.rename(columns={'subject_ids': 'subject_id'}, inplace=True)
-    df.sort_values(['subject_id', 'classification_id'], inplace=True)
+    unreconciled_df.drop(['user_id', 'user_ip', 'subject_id'], axis=1, inplace=True)
+    unreconciled_df.rename(columns={'subject_ids': 'subject_id'}, inplace=True)
+    unreconciled_df.sort_values(['subject_id', 'classification_id'], inplace=True)
 
-    if ARGS.unreconciled:
-        df.to_csv(ARGS.unreconciled, sep=',', index=False, encoding='utf-8')
-
-
-def output(best, match_type, count=None, total=None, score=None):
-    if match_type in [EXACT_MATCH, NORMALIZED_EXACT_MATCH]:
-        return '{} {}/{}{}{}'.format(match_type, count, total, SEPARATOR, best)
-    elif match_type in [PARTIAL_RATIO_MATCH, TOKEN_SET_RATIO_MATCH]:
-        return '{} score={}{}{}'.format(match_type, score, SEPARATOR, best)
-    return '{}{}{}'.format(match_type, SEPARATOR, best)
+    return unreconciled_df
 
 
 def normalize_text(group):
     return ['\n'.join([' '.join(ln.split()) for ln in g.splitlines()]) for g in group]
-
-
-def top_exact(values):
-    counts = [ExactScore(c[0], c[1]) for c in Counter(values).most_common()]
-    return counts[0] if counts[0].count > 1 else None
 
 
 def top_partial_ratio(group):
@@ -173,80 +164,106 @@ def top_token_set_ratio(group):
 
 def reconcile_same(group):
     values = [g for g in group]
-    counts = Counter(values)
+    # counts = Counter(values)
     # Remove columns where we expect identical values but do not get them
-    if len(counts) > 1:
-        COLUMNS_WITH_BAD_DATA[group.name] = 1
-        return ''
+    # if len(counts) > 1:
+    #     COLUMNS_WITH_BAD_DATA[group.name] = 1
+    #     return ''
     return values[0]
+
+
+def output(match_type, value, values=None, filled=None, score=None):
+    if match_type in [match_types.exact, match_types.normalized_exact]:
+        total = len(values)
+        blanks = total - reduce((lambda x, y: x + y.count), filled, 0)
+        word = 'blank' if blanks == 1 else 'blanks'
+        return '{} {}/{} with {} {}{}{}'.format(match_type, filled[0].count, total, blanks, word, SEPARATOR, value)
+    elif match_type in [match_types.select_no_match, match_types.text_no_match]:
+        total = len(values)
+        blanks = total - reduce((lambda x, y: x + y.count), filled, 0)
+        return '{} {}/{} with {} blanks{}{}'.format(match_type, 0, total, blanks, SEPARATOR, value)
+    elif match_type in [match_types.all_blank]:
+        total = len(values)
+        return '{} {}/{}{}{}'.format(match_type, total, total, SEPARATOR, value)
+    elif match_type in [match_types.one_transcript]:
+        total = len(values)
+        return '{} {}/{}{}{}'.format(match_type, 1, total, SEPARATOR, value)
+    elif match_type in [match_types.partial_ratio, match_types.token_set_ratio]:
+        return '{} score={}{}{}'.format(match_type, score, SEPARATOR, value)
+    print('Error unknown match type.')
+    sys.exit()
 
 
 def reconcile_select(group):
     values = [str(g) if str(g).lower() not in PLACE_HOLDERS else '' for g in group]
-    if len(values) < 2:
-        return output(NO_MATCHES, NOT_ENOUGH_TO_MATCH)
-    top = top_exact(values)
-    if top:
-        return output(top.value, EXACT_MATCH, count=top.count, total=len(values))
-    return output(NO_MATCHES, SELECT_NO_MATCH)
+    filled = [ExactScore(c[0], c[1]) for c in Counter([v for v in values if v]).most_common()]
+
+    if not filled:
+        return output(match_types.all_blank, '', values=values, filled=filled)
+
+    if filled[0].count > 1:
+        return output(match_types.exact, filled[0].value, values=values, filled=filled)
+
+    if len(filled) == 1:
+        return output(match_types.one_transcript, filled[0].value, values=values, filled=filled)
+
+    return output(match_types.select_no_match, NO_MATCHES, values=values, filled=filled)
 
 
 def reconcile_text(group):
     global ARGS
 
     values = normalize_text(group)
-    if len(values) < 2:
-        return output(NO_MATCHES, NOT_ENOUGH_TO_MATCH)
+    filled = [ExactScore(c[0], c[1]) for c in Counter([v for v in values if v]).most_common()]
 
-    # Look for identical matches and order them by how common they are
-    counts = [ExactScore(c[0], c[1]) for c in Counter(values).most_common()]
-    if (not counts[0].value and len(counts) == 1) or (counts[0].count > 1):
-        return output(counts[0].value, NORMALIZED_EXACT_MATCH, count=counts[0].count, total=len(values))
+    if not filled:
+        return output(match_types.all_blank, '', values=values, filled=filled)
+
+    if filled[0].count > 1:
+        return output(match_types.normalized_exact, filled[0].value, values=values, filled=filled)
+
+    if len(filled) == 1:
+        return output(match_types.one_transcript, filled[0].value, values=values, filled=filled)
 
     # Check for simple inplace fuzzy matches
     top = top_partial_ratio(group)
     if top.score >= ARGS.fuzzy_ratio_threshold:
-        return output(top.value, PARTIAL_RATIO_MATCH, score=top.score)
+        return output(match_types.partial_ratio, top.value, score=top.score)
 
     # Now look for the best token match
     top = top_token_set_ratio(group)
     if top.score >= ARGS.fuzzy_set_threshold:
-        return output(top.value, TOKEN_SET_RATIO_MATCH, score=top.score)
-    return output(NO_MATCHES, TEXT_NO_MATCH)
+        return output(match_types.token_set_ratio, top.value, score=top.score)
+
+    return output(match_types.text_no_match, NO_MATCHES, values=values, filled=filled)
 
 
 def reconcile(unreconciled_df):
-    global ARGS
-
     # How to aggregate the columns based on each column's type which is determined by the column name
     select_cols = {c: reconcile_select for c in unreconciled_df.columns if re.match(r'T\d+s:', c)}
     text_cols = {c: reconcile_text for c in unreconciled_df.columns if re.match(r'T\d+t:', c)}
     subject_cols = {c: reconcile_same for c in unreconciled_df.columns if c.startswith('subject_') and c != GROUP_BY}
     aggregate_cols = dict(list(select_cols.items()) + list(text_cols.items()) + list(subject_cols.items()))
-    aggregate_cols['locations'] = reconcile_same
+    aggregate_cols['locations'] = reconcile_same  # We want this column that is not in any of the above categories
     aggregate_cols = {k: v for k, v in aggregate_cols.items() if k not in UNWANTED_COLUMNS}
 
     # Aggregate using the per column functions setup above
-    grouped_df = unreconciled_df.fillna('').groupby(GROUP_BY).aggregate(aggregate_cols)
+    reconciled_df = unreconciled_df.fillna('').groupby(GROUP_BY).aggregate(aggregate_cols)
 
-    grouped_df.drop(COLUMNS_WITH_BAD_DATA.keys(), axis=1, inplace=True)
+    # reconciled_df.drop(COLUMNS_WITH_BAD_DATA.keys(), axis=1, inplace=True)
 
     # Split the combined reconciled value and flag into separate columns
     for c in list(text_cols.keys()) + list(select_cols.keys()):
-        grouped_df[c + '_explanation'], grouped_df[c] = grouped_df[c].str.split(SEPARATOR, n=1).str
+        reconciled_df[c + '_explanation'], reconciled_df[c] = reconciled_df[c].str.split(SEPARATOR, n=1).str
 
-    grouped_df = grouped_df.reindex_axis(sorted(grouped_df.columns), axis=1)
+    reconciled_df = reconciled_df.reindex_axis(sorted(reconciled_df.columns), axis=1)
+    explanations_df = reconciled_df.loc[:, [c for c in reconciled_df.columns if c.endswith('_explanation')]]
+    reconciled_df.drop([c for c in reconciled_df.columns if c.endswith('_explanation')], axis=1, inplace=True)
 
-    # Make the sidecar explanations file -- DELETE these lines if we want one file for this
-    explanations = grouped_df.loc[:, [c for c in grouped_df.columns if c.endswith('_explanation')]]
-    grouped_df.drop([c for c in grouped_df.columns if c.endswith('_explanation')], axis=1, inplace=True)
-    if ARGS.explanations:
-        explanations.to_csv(ARGS.explanations, sep=',', encoding='utf-8')
-
-    grouped_df.to_csv(ARGS.reconciled, sep=',', encoding='utf-8')
+    return reconciled_df, explanations_df
 
 
-if __name__ == "__main__":
+def parse_command_line():
     parser = argparse.ArgumentParser(description='''
         This takes raw Notes from Nature classifications and subjects files and creates a reconciliation
         of the classifications for a particular workflow. That is, it reduces n classifications per
@@ -282,17 +299,39 @@ if __name__ == "__main__":
                              '(default=reconciled_<workflow-id>_summary.txt).')
     parser.add_argument('-M', '--no-summary', action='store_true',
                         help='Do not write a summary file.')
-    ARGS = parser.parse_args()
-    if not ARGS.reconciled:
-        ARGS.reconciled = 'reconciled_{}.csv'.format(ARGS.workflow_id)
-    if not ARGS.explanations:
-        ARGS.explanations = 'reconciled_{}_explanations.csv'.format(ARGS.workflow_id)
-    if not ARGS.summary:
-        ARGS.explanations = 'reconciled_{}_summary.txt'.format(ARGS.workflow_id)
-    if ARGS.no_reconciled and not ARGS.unreconciled:
+    args = parser.parse_args()
+    if not args.reconciled:
+        args.reconciled = 'reconciled_{}.csv'.format(args.workflow_id)
+    if not args.explanations:
+        args.explanations = 'reconciled_{}_explanations.csv'.format(args.workflow_id)
+    if not args.summary:
+        args.summary = 'reconciled_{}_summary.txt'.format(args.workflow_id)
+    if args.no_reconciled and not args.unreconciled:
         print('The --no-reconciled option (-R) requires the --unreconciled (-u) option.')
         sys.exit()
+    args.explanations = '' if args.no_explanations else args.explanations
+    args.reconciled = '' if args.no_reconciled else args.reconciled
+    args.summary = '' if args.no_summary else args.summary
+    return args
 
-    unreconciled_df = expand()
-    if not ARGS.no_reconciled:
-        reconcile(unreconciled_df)
+
+if __name__ == "__main__":
+    ARGS = parse_command_line()
+
+    unreconciled_df = expand(ARGS.workflow_id, ARGS.input_classifications, ARGS.input_subjects)
+
+    if ARGS.unreconciled:
+        unreconciled_df.to_csv(ARGS.unreconciled, sep=',', index=False, encoding='utf-8')
+
+    if ARGS.no_reconciled:
+        sys.exit()
+
+    reconciled_df, explanations_df = reconcile(unreconciled_df)
+
+    reconciled_df.to_csv(ARGS.reconciled, sep=',', encoding='utf-8')
+
+    if ARGS.explanations:
+        explanations_df.to_csv(ARGS.explanations, sep=',', encoding='utf-8')
+
+    # if ARGS.summary:
+    #     summary_df.to_csv(ARGS.summary, sep=',', encoding='utf-8')
