a83ce9e5261990150e37086602b7f6537cad139e
rafe <raphael.lafrance at gmail.com>
2016-12-15 16:18:54 -0500

    Using a template for the summary report and added table guides
    Fixes #14, #16

--- summary_report.py
+++ summary_report.py
@@ -1,191 +1,108 @@
+"""Render a summary of the reconciliation process."""
+
 import re
 import sys
-import xml.etree.ElementTree as et
 from datetime import datetime
-from collections import Counter, namedtuple
+from jinja2 import Environment, PackageLoader
 import utils
 
-
-HTML_TEMPLATE = '''
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title />
-    <style>
-      header h1 { text-align: center; }
-      header div { width: 360px; margin-bottom: 4px; }
-      header div label { text-align: right; display: inline-block; width: 200px; }
-      header div span { float: right; }
-      section { margin-top: 2em; margin-bottom: 1em; }
-      th { padding: 2px 4px; }
-      #stats thead tr:nth-child(1) th:nth-child(2) { background: lightgray; }
-      #stats thead tr:nth-child(2) th { text-decoration: underline; }
-      #stats thead tr:nth-child(2) th:nth-child(1) { text-align: left; }
-      #stats thead tr:nth-child(2) th:nth-child(2) { text-align: left; }
-      #stats tr td:nth-child(3), #stats tr th:nth-child(3) { text-align: right; }
-      #stats tr td:nth-child(4), #stats tr th:nth-child(4) { text-align: right; }
-      #stats tr td:nth-child(5), #stats tr th:nth-child(5) { text-align: right; }
-      #stats tr td:nth-child(6), #stats tr th:nth-child(6) { text-align: right; }
-      #stats tr td:nth-child(7), #stats tr th:nth-child(7) { text-align: right; }
-      #stats tr td:nth-child(8), #stats tr th:nth-child(8) { text-align: right; }
-      #stats tr td:nth-child(1){ padding-left: 4px; }
-      #stats tr td:nth-child(2){ padding-left: 4px; }
-      #stats tr td:nth-child(3){ padding-right: 12px; }
-      #stats tr td:nth-child(4){ padding-right: 12px; }
-      #stats tr td:nth-child(5){ padding-right: 12px; }
-      #stats tr td:nth-child(6){ padding-right: 12px; }
-      #stats tr td:nth-child(7){ padding-right: 12px; }
-      #stats tr td:nth-child(8){ padding-right: 12px; }
-      #problems th { text-align: left; text-decoration: underline; }
-      #problems td { padding-left: 4px; }
-    </style>
-  </head>
-  <body>
-    <header>
-      <h1 id="title" />
-      <div><label>Date:</label><span id="date" /></div>
-      <div><label>Number of Subjects:</label><span id="subjects" /></div>
-      <div><label>Number of Transcripts:</label><span id="transcripts" /></div>
-      <div><label>Transcripts per Subject:</label><span id="ratio" /></div>
-    </header>
-    <section id="stats">
-      <h2>Reconciled Data</h2>
-      <table>
-        <thead>
-          <tr>
-            <th colspan="2"></th>
-            <th colspan="5">Reconciled</th>
-            <th colspan="1"></th>
-          </tr>
-          <tr>
-            <th>Field</th>
-            <th>Type</th>
-            <th>Exact Matches</th>
-            <th>Fuzzy Matches</th>
-            <th>All Blank</th>
-            <th>One Transcript</th>
-            <th>Total</th>
-            <th>No Matches</th>
-          </tr>
-        </thead>
-        <tbody />
-      </table>
-    </section>
-    <section id="problems">
-      <h2>Problem Records</h2>
-      <table>
-        <thead />
-        <tbody />
-      </table>
-    </section>
-  </body>
-</html>
-'''
+# These depend on the patterns put into explanations_df FIXME
+NO_MATCH_PATTERN = r'^No (?:select|text) match on'
+EXACT_MATCH_PATTERN = r'^(?:Exact|Normalized exact) match'
+FUZZ_MATCH_PATTERN = r'^(?:Partial|Token set) ratio match'
+ALL_BLANK_PATTERN = r'^(?:All|The) \d+ record'
+ONESIES_PATTERN = r'^Only 1 transcript in'
 
 
-def create_summary_report(unreconciled_df, reconciled_df, explanations_df, args):
-    html = et.fromstring(HTML_TEMPLATE)
+def get_workflow_name(args, unreconciled_df):
+    """Extract and format the workflow name from the dataframe."""
+
     try:
         workflow_name = unreconciled_df.loc[0, 'workflow_name']
         workflow_name = re.sub(r'^[^_]*_', '', workflow_name)
     except KeyError:
         print('Workflow ID {} not found in classifications file.'.format(args.workflow_id))
         sys.exit(1)
+    return workflow_name
 
-    html.find('.head/title').text = 'Summary of {}'.format(args.workflow_id)
-    workflow_name = unreconciled_df.loc[0, 'workflow_name'] if 'workflow_name' in unreconciled_df.columns else ''
-    workflow_name = re.sub(r'^[^_]*_', '', workflow_name)
 
-    html.find('.head/title').text = 'Summary of {}'.format(args.workflow_id)
+def header_data(args, reconciled_df, unreconciled_df):
+    """Data that goes into the report header."""
 
-    html.find(".//h1[@id='title']").text = 'Summary of "{}" ({})'.format(workflow_name, args.workflow_id)
-    html.find(".//span[@id='date']").text = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M')
-    html.find(".//span[@id='subjects']").text = '{:,}'.format(reconciled_df.shape[0])
-    html.find(".//span[@id='transcripts']").text = '{:,}'.format(unreconciled_df.shape[0])
-    html.find(".//span[@id='ratio']").text = '{:.2f}'.format(unreconciled_df.shape[0] / reconciled_df.shape[0])
+    workflow_name = get_workflow_name(args, unreconciled_df)
+    return {
+        'date': datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M'),
+        'title': 'Summary of {}'.format(args.workflow_id),
+        'ratio': '{:.2f}'.format(unreconciled_df.shape[0] / reconciled_df.shape[0]),
+        'heading': 'Summary of "{}" ({})'.format(workflow_name, args.workflow_id),
+        'subjects': '{:,}'.format(reconciled_df.shape[0]),
+        'transcripts': '{:,}'.format(unreconciled_df.shape[0]),
+        'workflow_name': workflow_name,
+    }
 
-    # These depend on the patterns put into explanations_df FIXME
-    no_match_pattern = r'^No (?:select|text) match on'
-    exact_match_pattern = r'^(?:Exact|Normalized exact) match'
-    fuzz_match_pattern = r'^(?:Partial|Token set) ratio match'
-    all_blank_pattern = r'^(?:All|The) \d+ record'
-    onesies_pattern = r'^Only 1 transcript in'
 
-    tbody = html.find(".//section[@id='stats']/table/tbody")
+def reconciled_summary(explanations_df):
+    """Build a summary of how each field was reconciled."""
+    reconciled = []
     for col in [c for c in explanations_df.columns]:
-        name = utils.format_name(col)
         col_type = 'select' if re.match(utils.SELECT_COLUMN_PATTERN, col) else 'text'
-        num_no_match = explanations_df[explanations_df[col].str.contains(no_match_pattern)].shape[0]
-        num_exact_match = explanations_df[explanations_df[col].str.contains(exact_match_pattern)].shape[0]
-        num_fuzzy_match = explanations_df[explanations_df[col].str.contains(fuzz_match_pattern)].shape[0]
-        num_all_blank = explanations_df[explanations_df[col].str.contains(all_blank_pattern)].shape[0]
-        num_onesies = explanations_df[explanations_df[col].str.contains(onesies_pattern)].shape[0]
-        num_reconciled = explanations_df.shape[0] - num_no_match
+        num_no_match = explanations_df[explanations_df[col].str.contains(NO_MATCH_PATTERN)].shape[0]
+        reconciled.append({
+            'name': utils.format_name(col),
+            'col_type': col_type,
+            'num_no_match': num_no_match,
+            'num_exact_match': '{:,}'.format(
+                explanations_df[explanations_df[col].str.contains(EXACT_MATCH_PATTERN)].shape[0]),
+            'num_fuzzy_match': '{:,}'.format(explanations_df[explanations_df[col].str.contains(
+                FUZZ_MATCH_PATTERN)].shape[0]) if col_type == 'text' else '',
+            'num_all_blank': '{:,}'.format(
+                explanations_df[explanations_df[col].str.contains(ALL_BLANK_PATTERN)].shape[0]),
+            'num_onesies': '{:,}'.format(
+                explanations_df[explanations_df[col].str.contains(ONESIES_PATTERN)].shape[0]),
+            'num_reconciled': '{:,}'.format(explanations_df.shape[0] - num_no_match),
+        })
+        return reconciled
 
-        tr = et.Element('tr')
 
-        td = et.Element('td')
-        td.text = name
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = col_type
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = '{:,}'.format(num_exact_match)
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = '{:,}'.format(num_fuzzy_match) if col_type == 'text' else ''
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = '{:,}'.format(num_all_blank)
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = '{:,}'.format(num_onesies)
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = '{:,}'.format(num_reconciled)
-        tr.append(td)
-
-        td = et.Element('td')
-        td.text = '{:,}'.format(num_no_match)
-        tr.append(td)
-
-        tbody.append(tr)
-
-    thead = html.find(".//section[@id='problems']/table/thead")
-    tr = et.Element('tr')
-    th = et.Element('th')
-    th.text = explanations_df.index.name
-    tr.append(th)
+def problem_thead_data(explanations_df):
+    """Get the header fields for the problem summary of each subject."""
+    problem_thead = []
+    problem_thead.append(explanations_df.index.name)
     for col in explanations_df.columns:
-        th = et.Element('th')
-        th.text = utils.format_name(col)
-        tr.append(th)
-    thead.append(tr)
+        problem_thead.append(utils.format_name(col))
+    return problem_thead
 
-    tbody = html.find(".//section[@id='problems']/table/tbody")
-    pattern = '|'.join([no_match_pattern, onesies_pattern])
+
+def problem_tbody_data(explanations_df):
+    """Get the data fields for the problem summary of each subject."""
+    problem_tbody = []
+
+    pattern = '|'.join([NO_MATCH_PATTERN, ONESIES_PATTERN])
     for subject_id, cols in explanations_df.iterrows():
-        tr = et.Element('tr')
-        td = et.Element('td')
-        td.text = str(subject_id)
-        tr.append(td)
+        trow = []
+        trow.append(str(subject_id))
         keep = False
         for col in cols:
-            td = et.Element('td')
+            tdata = ''
             if re.search(pattern, col):
                 keep = True
-                td.text = col
-            tr.append(td)
+                tdata = col
+            trow.append(tdata)
         if keep:
-            tbody.append(tr)
+            problem_tbody.append(trow)
+    return problem_tbody
 
-    with open(args.summary, 'wb') as out_file:
-        out_file.write('<!DOCTYPE html>\n'.encode())
-        out_file.write(et.tostring(html))
+
+def create_summary_report(unreconciled_df, reconciled_df, explanations_df, args):
+    """Build the report from the template."""
+    env = Environment(loader=PackageLoader('reconcile', '.'))
+    template = env.get_template('summary_report_template.html')
+    # pylint: disable=E1101
+    summary = template.render(header=header_data(args, reconciled_df, unreconciled_df),
+                              reconciled=reconciled_summary(explanations_df),
+                              problem_thead=problem_thead_data(explanations_df),
+                              problem_tbody=problem_tbody_data(explanations_df))
+    # pylint: enable=E1101
+
+    with open(args.summary, 'w') as out_file:
+        out_file.write(summary)
