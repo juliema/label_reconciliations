a883a21bf903673d68874179a963535ff038a8b4
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-03-22 14:22:56 -0400

    Add zip file options Fixes #25

--- reconcile.py
+++ reconcile.py
@@ -1,6 +1,9 @@
 """The main program."""
 
+import os
 import sys
+import zlib
+import zipfile
 import argparse
 from lib.unreconciled_builder import UnreconciledBuilder
 from lib.reconciled_builder import ReconciledBuilder
@@ -22,27 +25,41 @@ def parse_command_line():
     parser.add_argument('classifications',
                         help=('The Notes from Nature classifications CSV '
                               'input file.'))
+
     parser.add_argument('-w', '--workflow-id', type=int,
                         help=('The workflow to extract. Required if there is '
                               'more than one workflow in the classifications '
                               'file.'))
+
     parser.add_argument('-r', '--reconciled',
                         help=('Write the reconciled classifications to this '
                               'CSV file.'))
+
     parser.add_argument('-u', '--unreconciled',
                         help=('Write the unreconciled workflow '
                               'classifications to this CSV file.'))
+
     parser.add_argument('-s', '--summary',
                         help=('Write a summary of the reconciliation to this '
                               'HTML file.'))
+
     parser.add_argument('-f', '--fuzzy-ratio-threshold', default=90, type=int,
                         help=('Sets the cutoff for fuzzy ratio matching '
                               '(0-100, default=90). '
                               'See https://github.com/seatgeek/fuzzywuzzy.'))
+
     parser.add_argument('-F', '--fuzzy-set-threshold', default=50, type=int,
                         help=('Sets the cutoff for fuzzy set matching (0-100, '
                               'default=50). '
                               'See https://github.com/seatgeek/fuzzywuzzy.'))
+
+    parser.add_argument('-z', '--zip',
+                        help='Zip files and put them into this archive. '
+                             'Remove the uncompressed files afterwards.')
+
+    parser.add_argument('-Z', '--zip-keep',
+                        help='Zip files and put them into this archive. '
+                             'Keep the uncompressed files afterwards.')
 
     args = parser.parse_args()
 
@@ -57,27 +74,54 @@ def parse_command_line():
     return args
 
 
-if __name__ == "__main__":
-    ARGS = parse_command_line()
+def zip_files(args):
+    """Put results into a zip file."""
 
-    UNRECONCILED_DF = UnreconciledBuilder(ARGS.workflow_id,
-                                          ARGS.classifications).build()
-    if UNRECONCILED_DF.shape[0] == 0:
-        print('Workflow {} has no data.'.format(ARGS.workflow_id))
+    zip_file = args.zip if args.zip else args.zip_keep
+
+    with zipfile.ZipFile(zip_file, mode='w') as zippy:
+        if args.unreconciled:
+            zippy.write(args.unreconciled, compress_type=zipfile.ZIP_DEFLATED)
+        if args.reconciled:
+            zippy.write(args.reconciled, compress_type=zipfile.ZIP_DEFLATED)
+        if args.summary:
+            zippy.write(args.summary, compress_type=zipfile.ZIP_DEFLATED)
+
+    if args.zip_keep:
+        return
+
+    if args.unreconciled:
+        os.remove(args.unreconciled)
+    if args.reconciled:
+        os.remove(args.reconciled)
+    if args.summary:
+        os.remove(args.summary)
+
+
+if __name__ == "__main__":
+    args = parse_command_line()
+
+    unreconciled_df = UnreconciledBuilder(args.workflow_id,
+                                          args.classifications).build()
+    if unreconciled_df.shape[0] == 0:
+        print('Workflow {} has no data.'.format(args.workflow_id))
         sys.exit(1)
 
-    if ARGS.unreconciled:
-        util.output_dataframe(UNRECONCILED_DF, ARGS.unreconciled, index=False)
+    if args.unreconciled:
+        util.output_dataframe(unreconciled_df, args.unreconciled, index=False)
 
-    if ARGS.reconciled or ARGS.summary:
-        RECONCILED_DF, EXPLANATIONS_DF = ReconciledBuilder(
-            ARGS, UNRECONCILED_DF).build()
+    if args.reconciled or args.summary:
+        reconciled_df, explanations_df = ReconciledBuilder(
+            args, unreconciled_df).build()
 
-        if ARGS.reconciled:
-            util.output_dataframe(RECONCILED_DF, ARGS.reconciled)
+        if args.reconciled:
+            util.output_dataframe(reconciled_df, args.reconciled)
 
-        if ARGS.summary:
-            SummaryReport(ARGS,
-                          UNRECONCILED_DF,
-                          RECONCILED_DF,
-                          EXPLANATIONS_DF).report()
+        if args.summary:
+            SummaryReport(args,
+                          unreconciled_df,
+                          reconciled_df,
+                          explanations_df).report()
+
+    if args.zip or args.zip_keep:
+        zip_files(args)
