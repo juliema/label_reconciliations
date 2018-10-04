8bedea75aaa6d22d7c851f243e71a8de2e5563db
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-10-04 14:37:08 -0400

    Add arguments and explanation to the export bash script

--- nfn_export.bash
+++ nfn_export.bash
@@ -1,23 +1,50 @@
 #!/bin/bash
 
-USAGE="Usage: nfn_export.bash -v RECONCILER-VERSION -w WORKFLOW-ID -r RENAME-TO -c COPY-TO -e EXPORT-FILE-NAME"
+USAGE=$(cat <<'USAGE_END'
 
-while getopts "v:w:r:e:c:h" option; do
+  Usage: nfn_export.bash -v RECONCILER-VERSION -w WORKFLOW-ID -r RENAME-TO -a ARGS -c COPY-TO -e EXPORT-FILE-NAME
+
+  -h
+      Print this message and exit.
+
+  -v RECONCILER-VERSION
+      The version of reconcile.py. This gets inserted into output file names.
+
+  -w WORKFLOW-ID
+      The expedition's workflow ID. This will be prepended to output file names.
+
+  -r RENAME-TO
+      The output file names without the RECONCILER-VERSION or WORKFLOW-ID.
+
+  -a ARGS
+      Any extra arguments to pass to reconcile.py. If you have many, quote them.
+
+  -c COPY-TO
+      Copy the output files to this directory.
+
+  -e EXPORT-FILE-NAME
+      The raw input file that reconcile.py uses as input.
+.
+USAGE_END
+)
+
+while getopts "a:v:w:r:e:c:h" option; do
   case "${option}" in
     e) EXPORT=${OPTARG};;
     r) RENAME=${OPTARG};;
     v) VERSION=${OPTARG};;
     w) WORKFLOW=${OPTARG};;
     c) COPYTO=${OPTARG};;
-    h) echo $USAGE
+    a) XARGS=${OPTARG};;
+    h) echo "$USAGE"
        exit 1
   esac
 done
 
-if [ -z $EXPORT ] || [ -z $RENAME ] || [ -z $VERSION ] || [ -z $WORKFLOW ] || [ -z $COPYTO ]
+if [ -z $EXPORT ] || [ -z $RENAME ] || [ -z $VERSION ] || [ -z $WORKFLOW ]
 then
-  echo "All arguments are required."
-  echo $USAGE
+  echo "Arguments: -v, -w, -r, & -e are required."
+  echo "$USAGE"
   exit 1
 fi
 
@@ -35,6 +62,7 @@ echo "Name of Reconciled Folder: ${PREFIX}"
 # echo "Reconciled file            ${RECONCILE}"
 # echo "Summary file               ${SUMMARY}"
 # echo "Raw file                   ${RAW}"
+# echo "Copy to:                   ${COPYTO}"
 echo ""
 
 mkdir -p $DIR
@@ -42,6 +70,10 @@ mkdir -p $DIR
 cp $EXPORT $RAW
 cp data/NfN_Reconciliation_HelpV1.0.pdf $DIR
 
-python reconcile.py -w $WORKFLOW -r $RECONCILE -s $SUMMARY $RAW
+python reconcile.py $XARGS -w $WORKFLOW -r $RECONCILE -s $SUMMARY $RAW
 
-cp -r $DIR $COPYTO
+if [ -n "$COPYTO" ]
+then
+  echo "hello"
+  cp -r $DIR $COPYTO
+fi
