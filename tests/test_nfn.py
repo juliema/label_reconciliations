50fee75f47b11556853e854baaeef6e9d328c283
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2019-02-01 16:54:21 -0500

    Update requirements and tests for Travis

--- tests/test_nfn.py
+++ tests/test_nfn.py
@@ -1,38 +1,43 @@
 """Test functions in lib/formats/nfn.py."""
 
 from argparse import Namespace
-import unittest
 from unittest.mock import patch
 import pandas as pd
 import lib.formats.nfn as nfn
 
 
-class TestAtramPreprocessor(unittest.TestCase):
+def set_up():
+    """Setup mock dataframes."""
+    df1 = pd.read_csv('tests/data/nfn1.csv', dtype=str)
+    df2 = pd.read_csv('tests/data/nfn2.csv', dtype=str)
+    return df1, df2
 
-    def setUp(self):
-        self.df1 = pd.read_csv('tests/data/nfn1.csv', dtype=str)
-        self.df2 = pd.read_csv('tests/data/nfn2.csv', dtype=str)
+def test_get_workflow_id_01():
+    """It handles returns a given workflow ID."""
+    _, df2 = set_up()
+    args = Namespace(workflow_id='1001')
 
-    def test_get_workflow_id_given(self):
-        args = Namespace(workflow_id='1001')
+    workflow_id = nfn.get_workflow_id(df2, args)
 
-        workflow_id = nfn.get_workflow_id(self.df2, args)
+    assert workflow_id == '1001'
 
-        assert workflow_id == '1001'
+def test_get_workflow_id_unique():
+    """It finds a unique workflow ID."""
+    df1, _ = set_up()
+    args = Namespace(workflow_id=None)
 
-    def test_get_workflow_id_unique(self):
-        args = Namespace(workflow_id=None)
+    workflow_id = nfn.get_workflow_id(df1, args)
 
-        workflow_id = nfn.get_workflow_id(self.df1, args)
+    assert workflow_id == '1001'
 
-        assert workflow_id == '1001'
+@patch('lib.util.error_exit')
+def test_get_workflow_id_error(error_exit):
+    """It errors when there are multiple workflow IDs to choose."""
+    _, df2 = set_up()
+    args = Namespace(workflow_id=None)
 
-    @patch('lib.util.error_exit')
-    def test_get_workflow_id_error(self, error_exit):
-        args = Namespace(workflow_id=None)
+    nfn.get_workflow_id(df2, args)
 
-        nfn.get_workflow_id(self.df2, args)
-
-        error_exit.assert_called_once_with(
-            ('There are multiple workflows in this file. '
-             'You must provide a workflow ID as an argument.'))
+    error_exit.assert_called_once_with(
+        ('There are multiple workflows in this file. '
+         'You must provide a workflow ID as an argument.'))
