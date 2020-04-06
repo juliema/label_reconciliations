f2cf3a2461fbe2c73dca6d9c219c96adf236a2f7
rafe <raphael.lafrance@gmail.com>
2020-04-06 10:56:35 -0400

    Add a new "mean" column type

--- lib/column_types/mean.py
+++ lib/column_types/mean.py
@@ -1,11 +1,11 @@
-"""Get mean median and range for the group. Handle non-numeric characters."""
+"""Get mean and range for the group. Handle non-numeric characters."""
 
 import re
 import statistics as stats
 import inflect
 
 
-COLUMN = 'mmr'
+COLUMN = 'mean'
 
 P = inflect.engine().plural
 
@@ -27,17 +27,9 @@ def reconcile(group, args=None):  # pylint: disable=unused-argument
         return reason, ''
 
     mean = stats.mean(numbers)
-    mode = stats.mode(numbers)
-    mode_count = len([n for n in numbers if n == mode])
 
-    value = ('mean={:.2f}, mode={:.2f} (occurs {} {}) '
-             'range=[{:.2f}, {:.2f}]').format(
-                 mean,
-                 mode,
-                 mode_count,
-                 P('time', mode_count),
-                 min(numbers),
-                 max(numbers))
+    value = 'mean={:.2f} range=[{:.2f}, {:.2f}]'.format(
+        mean, min(numbers), max(numbers))
 
     reason = 'There {} {} {} in {} {}'.format(
         P('is', len(numbers)),
@@ -53,17 +45,15 @@ def adjust_reconciled_columns(reconciled, column_types):
                if column_types.get(c, {'type': ''})['type'] == COLUMN}
     for column in columns:
         reconciled[f'{column} Mean'] = reconciled[column].apply(
-            lambda x: _get_mmr_part(x, r'mean=([0-9.]+)'))
-        reconciled[f'{column} Mode'] = reconciled[column].apply(
-            lambda x: _get_mmr_part(x, r'mode=([0-9.]+)'))
+            lambda x: _get_mean_part(x, r'mean=([0-9.]+)'))
         reconciled[f'{column} Range lower'] = reconciled[column].apply(
-            lambda x: _get_mmr_part(x, r'range=\[([0-9.]+)'))
+            lambda x: _get_mean_part(x, r'range=\[([0-9.]+)'))
         reconciled[f'{column} Range upper'] = reconciled[column].apply(
-            lambda x: _get_mmr_part(x, r'range=\[[^,\s]+[,\s]*([0-9.]+)'))
+            lambda x: _get_mean_part(x, r'range=\[[^,\s]+[,\s]*([0-9.]+)'))
     return reconciled.drop(columns, axis='columns')
 
 
-def _get_mmr_part(value, regex):
+def _get_mean_part(value, regex):
     if not value:
         return ''
     match = re.search(regex, value)
